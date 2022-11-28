f98bad89826082354758219e261c1b70538c98e2
Vijay Chauhan <vijayc@techved.com>
2022-11-28 22:07:00 +0530

    working pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { LibraryComponent } from './library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
