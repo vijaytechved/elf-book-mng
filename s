4d1c3133acd1488092915d6b1bf17c36ff34ade6
Vijay Chauhan <vijayc@techved.com>
2022-12-16 10:51:35 +0530

    working]

---
+++ s
@@ -0,0 +1,29 @@
+[33mcommit 0db9cee5274cf1a2e6750376e637c4fc28e41ff3[m[33m ([m[1;36mHEAD -> [m[1;32mvijay-angular[m[33m)[m
+Author: Vijay Chauhan <vijayc@techved.com>
+Date:   Tue Dec 13 06:12:43 2022 +0530
+
+    working
+
+[33mcommit e16e8c1a6af35af8d00ec8397c066a94de0101bc[m[33m ([m[1;32mdev[m[33m)[m
+Author: Vijay Chauhan <vijayc@techved.com>
+Date:   Mon Dec 12 21:14:00 2022 +0530
+
+    working
+
+[33mcommit f98bad89826082354758219e261c1b70538c98e2[m[33m ([m[1;33mtag: v1.0[m[33m)[m
+Author: Vijay Chauhan <vijayc@techved.com>
+Date:   Mon Nov 28 22:07:00 2022 +0530
+
+    working pages
+
+[33mcommit a4249d60725a034814995fe9546a788b2b2a9952[m[33m ([m[1;32mmain[m[33m)[m
+Author: Vijay Chauhan <vijayc@techved.com>
+Date:   Mon Nov 28 21:12:09 2022 +0530
+
+    change text index
+
+[33mcommit 57f68dc659698fb8be0b76089d6b96546163ff55[m[33m ([m[1;32mmaster[m[33m)[m
+Author: Vijay Chauhan <vijayc@techved.com>
+Date:   Mon Nov 28 12:55:11 2022 +0530
+
+    initial commit
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -8,6 +8,23 @@ import {  Router } from '@angular/router';
 })
 export class BookListComponent implements OnInit {
   getGuard:any
+  books= [
+    {
+      name:"Test"
+    },
+    {
+      name:"Test2"
+    },
+    {
+      name:"Test3"
+    },
+    {
+      name:"Test4"
+    },
+    {
+      name:"Test5"
+    }
+  ]
   constructor(private router:Router) { }
 
   ngOnInit(): void {
@@ -15,9 +32,41 @@ export class BookListComponent implements OnInit {
     if(this.getGuard){
       this.router.navigate(['book-list'])
     }
+
   }
 
+  modifyArray(){
+    let b = this.books
+    b.forEach(ele=>{
+    if(ele.name === 'Test2'){
+      ele.name = '0' 
+    }
+    })
+    let p:any =b
+  //   p = b.push(p)
+  // b.map((element:any)=>{
+  //   if(element.name === 'Test2'){
+  //     Object.assign(element.name, {"name":'0'})
+  //   }   
+  // })
+  b.map((x)=>{
+    if(x.name.includes("Test2")){
+      x['name'] = x.name.replace("Test2","0");
+      this.books
+    } else{
+      this.books
+    }
+  })
+  // allData.forEach(element => {
+  //   this.listDate.push(element)
+  // });
+  // p.forEach(w=>{
+  //   this.books.push(w)
+  // })
+  // this.books.push(p)
+  //   console.log(p)
 
+   }
 
 
 }
